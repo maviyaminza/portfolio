6c6380b072c8b802ba1990f93189b26f228389c4
Maviya Minza <maviyaminza@gmail.com>
2026-08-05 20:42:07 +0530

    Initial portfolio website

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// ============================
+// Mobile Menu
+// ============================
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// ============================
+// Scroll Animation
+// ============================
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+
+    threshold:0.15
+
+});
+
+sections.forEach((section)=>{
+
+    section.classList.add("hidden");
+
+    observer.observe(section);
+
+});
